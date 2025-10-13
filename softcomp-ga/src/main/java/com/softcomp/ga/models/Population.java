58c712bbe9d0df9ad20b9f6b9ba5c7a4506803f6
YousefSher <yousefsmn@gmail.com>
2025-10-13 22:03:42 +0300

    rollete wheel selection

--- softcomp-ga/src/main/java/com/softcomp/ga/models/Population.java
+++ softcomp-ga/src/main/java/com/softcomp/ga/models/Population.java
@@ -3,7 +3,13 @@ package com.softcomp.ga.models;
 import java.util.List;
 
 public class Population<T> {
-    private List< Individual<T> > individuals;
+    private List<Individual<T>> individuals;
 
+    public Individual<T> getIndividualOfIndex(int index) {
+        return individuals.get(index);
+    }
 
+    public int getPopulationSize() {
+        return individuals.size();
+    }
 }
--- softcomp-ga/src/main/java/com/softcomp/ga/selection/RouletteSelection.java
+++ softcomp-ga/src/main/java/com/softcomp/ga/selection/RouletteSelection.java
@@ -1,16 +1,33 @@
 package com.softcomp.ga.selection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.softcomp.ga.models.Chromosome;
 import com.softcomp.ga.models.Population;
 
-public class RouletteSelection<T> implements ISelection<T> { // our case study will use it
+public class RouletteSelection<T> implements ISelection<T> {
+    protected Random random = new Random();
 
     @Override
-    public Chromosome<T> select(Population<T> population){
+    public Chromosome<T> select(Population<T> population) {
 
-        // implementation
-
-        return null;
+        Chromosome<T> chosen = population.getIndividualOfIndex(0).getChromosome();
+        int populationSize = population.getPopulationSize(), randomNumber, sum = 0;
+        List<Integer> comulativeFitness = new ArrayList<>();
+        for (int i = 0; i < populationSize; i++) {
+            sum += population.getIndividualOfIndex(i).getFitness();
+            comulativeFitness.set(i, sum);
+        }
+        randomNumber = random.nextInt(sum);
+        for (int i = 0; i < comulativeFitness.size(); i++) {
+            if (comulativeFitness.get(i) > randomNumber) {
+                chosen = population.getIndividualOfIndex(i).getChromosome();
+                break;
+            }
+        }
+        return chosen;
     }
 
 }
