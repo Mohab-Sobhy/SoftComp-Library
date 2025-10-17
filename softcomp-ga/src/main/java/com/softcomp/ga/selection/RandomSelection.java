2a96f36bc0d46addcce95e4094e397c0aa5f5e76
Khaledslore <khaledmohcs@gmail.com>
2025-10-17 22:59:13 +0300

    pls work

--- softcomp-ga/src/main/java/com/softcomp/ga/selection/RandomSelection.java
+++ softcomp-ga/src/main/java/com/softcomp/ga/selection/RandomSelection.java
@@ -1,16 +1,21 @@
 package com.softcomp.ga.selection;
 
+import java.util.Random;
 import com.softcomp.ga.models.Chromosome;
 import com.softcomp.ga.models.Population;
 
 public class RandomSelection<T> implements ISelection<T> {
 
+    private Random random = new Random();
+
     @Override
-    public Chromosome<T> select(Population<T> population){
+    public Chromosome<T> select(Population<T> population) {
+        int populationSize = population.getPopulationSize();
+        if (populationSize == 0) {
+            return null;
+        }
 
-        // implementation
-
-        return null;
+        int randomIndex = random.nextInt(populationSize);
+        return population.getIndividualOfIndex(randomIndex).getChromosome();
     }
-
 }
--- softcomp-ga/src/main/java/com/softcomp/ga/selection/TournamentSelection.java
+++ softcomp-ga/src/main/java/com/softcomp/ga/selection/TournamentSelection.java
@@ -1,16 +1,44 @@
 package com.softcomp.ga.selection;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import com.softcomp.ga.models.Chromosome;
+import com.softcomp.ga.models.Individual;
 import com.softcomp.ga.models.Population;
 
 public class TournamentSelection<T> implements ISelection<T> {
 
+    private int tournamentSize;
+    private Random random = new Random();
+
+    public TournamentSelection(int tournamentSize) {
+        this.tournamentSize = tournamentSize;
+    }
+
     @Override
-    public Chromosome<T> select(Population<T> population){
+    public Chromosome<T> select(Population<T> population) {
+        int populationSize = population.getPopulationSize();
+        if (populationSize == 0) {
+            return null; // just in case empty population
+        }
 
-        // implementation
+        List<Individual<T>> participants = new ArrayList<>();
 
-        return null;
+        for (int i = 0; i < tournamentSize; i++) {
+            int randomIndex = random.nextInt(populationSize);
+            participants.add(population.getIndividualOfIndex(randomIndex));
+        }
+
+        Individual<T> winner = participants.get(0);
+
+        for (int i = 1; i < tournamentSize; i++) {
+            if (participants.get(i).getFitness() > winner.getFitness()) {
+                winner = participants.get(i);
+            }
+        }
+
+        return winner.getChromosome();
     }
 
 }
