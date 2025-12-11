48319e5a51763eb56291237255718bcf20998097
YousefSher <yousefsmn@gmail.com>
2025-12-11 14:25:54 +0200

    sigmoid and relu but will fix the backwards later

--- softcomp-nn/src/main/java/com/softcomp/nn/activaitionfunction/ActivationFunction.java
+++ softcomp-nn/src/main/java/com/softcomp/nn/activaitionfunction/ActivationFunction.java
@@ -1,5 +1,7 @@
 package com.softcomp.nn.activaitionfunction;
 
+//make sure the derivative is used for all activation functions
+//Sigmoid, ReLU, Tanh, Linear.
 public interface ActivationFunction {
     double[] forward(double[] z);
 
